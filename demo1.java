3c7fc3c6b78a5799a66ebaac69d8a3cdb800212b
shravani8005/ <shravani8005@gmail.com>
2025-04-23 23:23:30 -0700

    save file

--- demo1.java
+++ demo1.java
@@ -1,12 +1,10 @@
 public class demo1 {
     
-        public static void main(String[] args) {
-            int a=20; int b=40;
-            System.out.println("sum is" + (a+b));
-            System.out.println("subtraction is" + (a-b));
-            
-            
-    
+    public static void main(String[] args) {
+        int a=20; int b=40;
+        System.out.println("sum is" + (a+b));
+        System.out.println("subtraction is" + (a-b));
+        System.out.println("mult is" + (a*b));
+        System.out.println("division is" + (a/b));
         }
-    }
-
+}
