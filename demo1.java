70c0c85408e07ad9b443906e37f15dc540f90d6e
shravani8005/ <shravani8005@gmail.com>
2025-04-23 23:13:37 -0700

    save file

---
+++ demo1.java
@@ -0,0 +1,12 @@
+public class demo1 {
+    
+        public static void main(String[] args) {
+            int a=20; int b=40;
+            System.out.println("sum is" + (a+b));
+            System.out.println("subtraction is" + (a-b));
+            
+            
+    
+        }
+    }
+
